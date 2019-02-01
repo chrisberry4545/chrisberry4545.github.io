eaa721da540f57f69703a8fc3e249b49b3abae4c
chrisberry4545 <chrisberry4545@googlemail.com>
2019-02-01 09:02:57 +0000

    added docs folder

---
+++ docs/precache-manifest.2619bfbc75dd892ee0d59234d58749cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2041e084fb9739b37103",
+    "url": "/static/js/main.2041e084.chunk.js"
+  },
+  {
+    "revision": "2340aeaefaf6c3e058b2",
+    "url": "/static/js/1.2340aeae.chunk.js"
+  },
+  {
+    "revision": "2041e084fb9739b37103",
+    "url": "/static/css/main.f951627a.chunk.css"
+  },
+  {
+    "revision": "3d4350e3d7cffd96ab2be77b26419524",
+    "url": "/index.html"
+  }
+];
